7889004e2973c2130257ac7b45c3886da2c8d96d
Ollivier Robert <roberto@keltia.net>
2015-08-14 23:47:38 +0200

    Use config & flag.

--- erc-search.go
+++ erc-search.go
@@ -1,1 +1,49 @@
-package erc_search
+// erc-search.go
+//
+// Small interface to get information from our corporate LDAP
+//
+// Copyright 2015 © by Ollivier Robert <roberto@keltia.net>
+
+/*
+ Package main implement a basic wrapper around LDAP search function.
+ */
+
+package main
+
+import (
+	"flag"
+	"os"
+	"path/filepath"
+	"log"
+	"github.com/keltia/erc-search/config"
+	"github.com/go-ldap/ldap"
+	"fmt"
+)
+
+const (
+)
+
+var (
+	RcFile = filepath.Join(os.Getenv("HOME"), ".erc-search", "config.yml")
+)
+
+func main () {
+	config, err := config.LoadConfig(RcFile)
+	if err != nil {
+		log.Printf("Warning: can not load %s\n", RcFile)
+	}
+	flag.Parse()
+
+	if flag.Arg(0) == "" {
+		log.Fatalln("Error: You must specify a search string")
+	}
+
+	site := fmt.Sprintf("%s:%d", config.Site, config.Port)
+	c, err := ldap.Dial("tcp", site);
+	if err != nil {
+		log.Fatalf("Error: Can't connect to %s\n", config.Site)
+	}
+
+	c.Close()
+}
+
