20dcf4e1df6a4509e2ca838f51ec2e15ba5457f1
Ollivier Robert <roberto@keltia.net>
2015-08-15 00:02:25 +0200

    Add -v for verbosity.

--- cli.go
+++ cli.go
@@ -15,9 +15,11 @@ import (
 var (
 	fInclFull bool
 	fInclMail bool
+	fVerbose  bool
 )
 
 func init() {
 	flag.BoolVar(&fInclFull, "F", false, "Include full name search")
 	flag.BoolVar(&fInclMail, "M", false, "Include mail search")
+	flag.BoolVar(&fVerbose, "v", false, "Be verbose")
 }
