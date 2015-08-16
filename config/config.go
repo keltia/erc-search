e4fc35d8aed5f8bb2b9975fb2055d811a210b850
Ollivier Robert <roberto@keltia.net>
2015-08-16 17:44:01 +0200

    Move from yaml to toml as main file format. Rationalize naming.

--- config/config.go
+++ config/config.go
@@ -6,13 +6,17 @@
 /*
  Package implement my homemade configuration class
 
- Looks into a YAML file for configuration options and returns a config.Config
- struct.
+ Looks into a YAML file for configuration options and returns a config.Config struct.
 
  	import "config"
 
- 	rc := config.LoadConfig("foo.yml")
+ 	rc := config.LoadConfig("tag")
 
+ 	or
+
+    rc := config.LoadConfig("foo.yml")
+
+ In the first case, $HOME/.tag/config.yml will be loaded.  On Windows
  rc will be serialized from YAML.
  */
 package config
@@ -20,47 +24,59 @@ package config
 import (
 	"io/ioutil"
 
-	"gopkg.in/yaml.v2"
 	"log"
 	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/naoina/toml"
 )
 
 type Config struct {
-	Site       string
-	Port       int
-	LdapBase   string
-	LdapFilter string
-	Attrs      []string
+	Site   string
+	Port   int
+	Base   string
+	Filter string
+	Attrs  []string
 }
 
 // Basic Stringer for Config
 func (c *Config) String() string {
 	return fmt.Sprintf("ldap://%s:%d/%s\n  Filter: %s\n  Attrs: %v",
-	c.Site, c.Port, c.LdapBase, c.LdapFilter, c.Attrs)
+	c.Site, c.Port, c.Base, c.Filter, c.Attrs)
 }
 
-// Load a file as a YAML document and return the structure
-func LoadConfig(file string) (Config, error) {
+// Load a file as a TOML document and return the structure
+func LoadConfig(file string) (*Config, error) {
+	// Check for tag
+	if _, err := os.Stat(file); err != nil {
+		// file must be a tag so add a "."
+		file = fmt.Sprintf(".%s", file)
+		file = filepath.Join(os.Getenv("HOME"),
+						     file,
+			                 "config.toml")
+	}
+
 	buf, err := ioutil.ReadFile(file)
 	if err != nil {
 		return Config{}, err
 	}
 
 	c := new(Config)
-	err = yaml.Unmarshal(buf, &c)
+	err = toml.Unmarshal(buf, &c)
 	if err != nil {
-		log.Println("Error parsing yaml")
+		log.Println("Error parsing toml %s", file)
 		return Config{}, err
 	}
 
-	return *c, err
+	return c, err
 }
 
 // Set defaults
 func (c *Config) SetDefaults() {
-	c.Site =     DEF_SERVER
-	c.Port =     DEF_PORT
-	c.LdapBase = DEF_BASE
-	c.LdapFilter = DEF_FILTER
-	c.Attrs =      DEF_ATTRS
+	c.Site   = DEF_SERVER
+	c.Port   = DEF_PORT
+	c.Base   = DEF_BASE
+	c.Filter = DEF_FILTER
+	c.Attrs  = DEF_ATTRS
 }
