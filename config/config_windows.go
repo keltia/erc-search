e2f34ff82ed7fb6886b08a74889de7f3a56387d4
Ollivier Robert <roberto@keltia.net>
2017-08-18 13:24:55 +0200

    Windows version.

---
+++ config/config_windows.go
@@ -0,0 +1,25 @@
+// config.go
+//
+// Copyright 2015 © by Ollivier Robert <roberto@keltia.net>
+//
+
+package config
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Check the parameter for either tag or filename
+func checkName(file string) string {
+	// Check for tag
+	if !strings.HasSuffix(file, ".toml") {
+		// file must be a tag so add a "."
+		return filepath.Join(os.Getenv("%LOCALAPPDATA%"),
+			"erc-search",
+			"config.toml")
+	}
+	return file
+}
