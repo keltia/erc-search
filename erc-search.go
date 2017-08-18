bf9e614fb0826484e4183a896cfa2cd25c173a8d
Ollivier Robert <roberto@keltia.net>
2017-08-18 11:09:42 +0200

    Split into machines/people.

--- erc-search.go
+++ erc-search.go
@@ -13,7 +13,6 @@ package main
 import (
 	"flag"
 	"github.com/keltia/erc-search/config"
-	myldap "github.com/keltia/erc-search/lib"
 	"log"
 )
 
@@ -33,61 +32,6 @@ type context struct {
 func (ctx *context) NewSource(name string) (config.Source) {
 	// Do the actual connect
 	return ctx.cnf.Sources[name]
-}
-
-// searchForPeople looks into the corporate LDAP
-func searchForPeople(ctx *context, text string) {
-	// Do the actual connect
-	src := ctx.NewSource("corporate")
-	log.Printf("Source: %v CNF: %v", src, ctx.cnf)
-	server, err := myldap.NewServer(src)
-	if err != nil {
-		log.Fatalf("Error: can not connect to %s: %v", src.Site, err)
-	}
-	defer server.Close()
-
-	if ctx.verbose {
-		server.SetVerbose(true)
-	}
-
-	// Minimum search is uid
-	attrs := map[string]bool{
-		"kgivenname": true,
-		"ksn":        true,
-		"uid":        true,
-		"eurocontrolworkstation": true,
-	}
-
-	// Setup searches
-	if fInclMail {
-		attrs["mail"] = true
-	}
-
-	// Meat of the game, the search
-	res, err := server.Search(attrs, flag.Arg(0))
-	if err != nil {
-		log.Printf("Error: searching failed: %v", err)
-	}
-
-	for _, entry := range res {
-		entry.PrettyPrint(2)
-	}
-	log.Printf("Found %d results\n", len(res))
-}
-
-// searchForMachine looks into AD for computers
-func searchForMachine(ctx *context, name string) {
-	src := ctx.NewSource("ad")
-	myad, err := myldap.NewServer(src)
-	if err != nil {
-		log.Fatalf("Error: can not connect to %s: %s", src.Site, err.Error())
-	}
-	defer myad.Close()
-
-	if ctx.verbose {
-		myad.SetVerbose(true)
-	}
-
 }
 
 // Start here
