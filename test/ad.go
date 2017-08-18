50440d37d6b19d0425e14ba5300b2f8beb4e9577
Ollivier Robert <roberto@keltia.net>
2017-08-18 11:12:30 +0200

    Make the testing app use the module.

--- test/ad.go
+++ test/ad.go
@@ -3,7 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
-	"net"
+	myldap "github.com/keltia/erc-search/lib"
 	"os"
 )
 
@@ -12,37 +12,8 @@ var (
 	ErrBadSRVRecord = errors.New("bad/empty SRV record")
 )
 
-// GetServerName returns one server amongst all SRV records
-func GetServerName(domain string) (srv string, err error) {
-	if domain == "" {
-		srv = domain
-		err = ErrEmptyDomain
-		return
-	}
-
-	nss, err := net.LookupNS(domain)
-	fmt.Printf("nss: %v\n", nss)
-	if err != nil {
-		srv = ""
-		return
-	}
-
-	// Get the actual SRV records
-	cname, addrs, err := net.LookupSRV("ldap", "tcp", domain)
-	fmt.Printf("cname: %+v srv: %+v\n", cname, addrs)
-	if err != nil {
-		srv = ""
-		return
-	}
-
-	// We suppose the nameserver does the randomize itself
-	srv = addrs[0].Target
-	return
-}
-
 func main() {
-	fmt.Printf("ping/foobar\n")
-	srv, err := GetServerName("sky.eurocontrol.int.")
+	srv, err := myldap.GetServerName("sky.corp.eurocontrol.int.")
 	if err != nil {
 		fmt.Printf("%+v - srv %+v\n", err, srv)
 		os.Exit(2)
