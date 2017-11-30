869e41ffe2a354ceb2a308f12744f950d221e47f
Ollivier Robert <roberto@keltia.net>
2017-11-30 17:36:14 +0100

    Remove unused import.

--- config_windows.go
+++ config_windows.go
@@ -8,12 +8,12 @@ package main
 import (
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 var (
 	basedir = filepath.Join(os.Getenv("%LOCALAPPDATA%"), rcFile)
 )
+
 /*
 File location: %LOCALAPPDATA%\ERC-SEARCH\
 */
